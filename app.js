22c78fd468576a8b846424da45b51731d10e38b0
Astrian <mingriqd@gmail.com>
2019-01-11 21:14:34 +0800

    where is ctx?

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ bot.command('toggle', (ctx) => {
   debug(config.ifttt)
   axios.post(`https://maker.ifttt.com/trigger/${config.ifttt.commands.toggle}/with/key/${config.ifttt.token}`, {})
   .then((response) => {
+    debug(ctx)
     ctx.reply('操作完成。')
   })
   .catch((error) => {
